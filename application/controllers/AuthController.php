f721b6b850db74f566104b43d07360d6923246ba
Usukhbaatar <osohoo02@gmail.com>
2014-03-13 22:40:29 +0800

    Sublime init

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Zend_Controller_Action {
 	}
 
 	public function indexAction() {
-		// action body
+		
 	}
 
 	public function loginAction() {
@@ -22,6 +22,7 @@ class AuthController extends Zend_Controller_Action {
 		}
 		$thefor = $this -> _request -> getParam('thefor');
 		$this -> view -> thefor = $thefor;
+
 		$form = new Form_Login();
 		$request = $this -> getRequest();
 		if ($request -> isPost()) {
