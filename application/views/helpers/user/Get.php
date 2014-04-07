a558a232ecbf0866f612197d46d04040a32f63d1
Usukhbaatar <osohoo02@gmail.com>
2014-04-07 14:58:59 +0900

    some deleted files

--- application/views/helpers/user/Get.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class View_Helper_User_Get extends Zend_View_Helper_Abstract {
-	public function constantReturn() {
-		$user = new Model_DbTable_Users();
-		return $user -> fetchAll();
-	}
-
-}
